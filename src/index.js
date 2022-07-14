5871b3758ea93ab654d36c567ab0ac2a70dff1e4
Sofia Versoza <92960864+sofversoza@users.noreply.github.com>
2022-07-13 17:29:14 -0700

    Home page done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-// import "./styles.css";
+// MARQUEE TEXT FOR INTRO
+function makeMarquee() {
+  const title = "Mon Beau Monde Museum in Bordeaux";
+  // using Array() constructor to create our title 50 times using the fill method, joined by a hyphen
+  const marqueeText = new Array(50).fill(title).join(" — ");
 
-// document.getElementById("app").innerHTML = `
-// <h1>Hello Vanilla!</h1>
-// <div>
-//   We use the same configuration as Parcel to bundle this sandbox, you can find more
-//   info about Parcel
-//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-// </div>
-// `;
+  // grab our .marquee ( name of div ) span ( title ) from html file
+  const marquee = document.querySelector(".marquee span");
+  // set our repeating title as the content ( using innerHTML lets us set the content inside an element )
+  marquee.innerHTML = marqueeText;
+}
+
+makeMarquee();
